e037123c74e809eb626edb58ca4479512c87f977
Antonio Linari <advancedlogic@me.com>
2015-06-07 09:48:45 +0200

    fixed imports paths

--- gofreeling.go
+++ gofreeling.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/advancedlogic/go-freeling/engine"
-	"github.com/advancedlogic/go-freeling/net"
-	"github.com/advancedlogic/go-freeling/terminal"
+	. "github.com/advancedlogic/go-freeling/engine"
+	. "github.com/advancedlogic/go-freeling/net"
+	. "github.com/advancedlogic/go-freeling/terminal"
 )
 
 var logo = `
